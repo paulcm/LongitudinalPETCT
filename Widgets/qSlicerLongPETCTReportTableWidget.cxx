ef71419bb3e78d8599f269aa461d415248558086
Paul <Paul@Pauls-MacBook-Pro.local>
2012-08-13 00:07:58 -0400

    ENH: Included ReportTable basic version

---
+++ Widgets/qSlicerLongPETCTReportTableWidget.cxx
@@ -0,0 +1,194 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+// LongPETCTReportTable Widgets includes
+#include "qSlicerLongPETCTReportTableWidget.h"
+
+
+#include <vtkMRMLLongPETCTReportNode.h>
+#include <vtkMRMLLongPETCTStudyNode.h>
+
+#include <QDate>
+
+//-----------------------------------------------------------------------------
+/// \ingroup Slicer_QtModules_LongitudinalPETCT
+class qSlicerLongPETCTReportTableWidgetPrivate
+  : public QTableWidget
+{
+  Q_DECLARE_PUBLIC(qSlicerLongPETCTReportTableWidget);
+protected:
+  qSlicerLongPETCTReportTableWidget* const q_ptr;
+
+public:
+  qSlicerLongPETCTReportTableWidgetPrivate(
+    qSlicerLongPETCTReportTableWidget& object);
+
+  virtual ~qSlicerLongPETCTReportTableWidgetPrivate();
+  virtual void setupUi(qSlicerLongPETCTReportTableWidget*);
+
+
+};
+
+// --------------------------------------------------------------------------
+qSlicerLongPETCTReportTableWidgetPrivate
+::qSlicerLongPETCTReportTableWidgetPrivate(
+  qSlicerLongPETCTReportTableWidget& object)
+  : q_ptr(&object)
+{
+}
+
+// --------------------------------------------------------------------------
+qSlicerLongPETCTReportTableWidgetPrivate
+::~qSlicerLongPETCTReportTableWidgetPrivate()
+{
+}
+
+
+// --------------------------------------------------------------------------
+void qSlicerLongPETCTReportTableWidgetPrivate
+::setupUi(qSlicerLongPETCTReportTableWidget* widget)
+{
+  Q_Q(qSlicerLongPETCTReportTableWidget);
+  widget->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Preferred);
+}
+
+//-----------------------------------------------------------------------------
+// qSlicerLongPETCTReportTableWidget methods
+
+//-----------------------------------------------------------------------------
+qSlicerLongPETCTReportTableWidget
+::qSlicerLongPETCTReportTableWidget(QWidget* parentWidget)
+  : Superclass( parentWidget )
+  , d_ptr( new qSlicerLongPETCTReportTableWidgetPrivate(*this) )
+{
+  Q_D(qSlicerLongPETCTReportTableWidget);
+  d->setupUi(this);
+}
+
+//-----------------------------------------------------------------------------
+qSlicerLongPETCTReportTableWidget
+::~qSlicerLongPETCTReportTableWidget()
+{
+}
+
+
+//-----------------------------------------------------------------------------
+void qSlicerLongPETCTReportTableWidget
+::update(vtkMRMLNode* node)
+{
+  Q_D(qSlicerLongPETCTReportTableWidget);
+
+  vtkSmartPointer<vtkMRMLLongPETCTReportNode> selectedReportNode = vtkMRMLLongPETCTReportNode::SafeDownCast(node);
+
+  if(selectedReportNode.GetPointer() == NULL)
+    return;
+
+  this->clear();
+  while (this->columnCount() > 0)
+    this->removeColumn(this->columnCount()-1);
+  while (this->rowCount() > 0)
+    this->removeRow(this->rowCount()-1);
+
+  this->insertColumn(0);
+  this->setHorizontalHeaderItem(0, new QTableWidgetItem(""));
+
+  int selectedStudies = selectedReportNode->GetSelectedStudiesCount();
+
+  for(int i=0; i < selectedStudies; ++i)
+    {
+      vtkSmartPointer<vtkMRMLLongPETCTStudyNode> study = selectedReportNode->GetSelectedStudy(i);
+
+      if(study.GetPointer() == NULL)
+        return;
+
+      int newColumnID = this->columnCount();
+
+      this->insertColumn(newColumnID);
+
+      QDate date = QDate::fromString(QString(study->GetAttribute("DICOM.StudyDate")).trimmed(),"yyyyMMdd");
+      QString itemText = date.toString(Qt::SystemLocaleShortDate);
+      QTableWidgetItem* item = new QTableWidgetItem(itemText);
+      item->setToolTip(itemText);
+      this->setHorizontalHeaderItem(newColumnID, item);
+    }
+
+  int lastSelectedIndex = selectedReportNode->GetIndexOfSelectedStudy(selectedReportNode->GetUserSelectedStudy());
+
+  if(lastSelectedIndex >= 0 && lastSelectedIndex < selectedReportNode->GetSelectedStudiesCount())
+    this->selectStudyColumn(lastSelectedIndex);
+
+  this->arrangeColumns();
+}
+
+
+//-----------------------------------------------------------------------------
+void qSlicerLongPETCTReportTableWidget
+::selectStudyColumn(int index)
+{
+  QFont font;
+
+  for(int i=0; i < this->columnCount();++i)
+    {
+      font = this->horizontalHeaderItem(i)->font();
+      font.setBold(false);
+
+      this->horizontalHeaderItem(i)->setFont(font);
+    }
+
+
+  if(index >= 0 && index < this->columnCount()-1)
+    {
+      font = this->horizontalHeaderItem(index+1)->font();
+      font.setBold(true);
+
+      this->horizontalHeaderItem(index+1)->setFont(font);
+    }
+
+}
+
+//-----------------------------------------------------------------------------
+void
+qSlicerLongPETCTReportTableWidget::arrangeColumns()
+{
+  int width = this->width() - 4;
+  int columns = this->columnCount();
+
+  for (int i = 0; i < columns; ++i)
+    {
+      int colWidth = static_cast<int>(width / columns);
+      if (colWidth < 30)
+        colWidth = 30;
+
+      this->setColumnWidth(i, colWidth);
+    }
+}
+
+//-----------------------------------------------------------------------------
+void qSlicerLongPETCTReportTableWidget
+::resizeEvent(QResizeEvent* event)
+{
+  Superclass::resizeEvent(event);
+  this->arrangeColumns();
+}
+
+
+
+
+
---
+++ Widgets/qSlicerLongPETCTReportTableWidget.h
@@ -0,0 +1,70 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+
+  Copyright (c) Kitware Inc.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+  This file was originally developed by Jean-Christophe Fillion-Robin, Kitware Inc.
+  and was partially funded by NIH grant 3P41RR013218-12S1
+
+==============================================================================*/
+
+#ifndef __qSlicerLongPETCTReportTableWidget_h
+#define __qSlicerLongPETCTReportTableWidget_h
+
+// Qt includes
+#include <QTableWidget>
+
+// LongPETCTReportTable Widgets includes
+#include "qSlicerLongPETCTModuleWidgetsExport.h"
+#include <vtkSmartPointer.h>
+
+class qSlicerLongPETCTReportTableWidgetPrivate;
+
+class vtkMRMLNode;
+
+
+
+/// \ingroup Slicer_QtModules_LongitudinalPETCT
+class Q_SLICER_LONGPETCT_MODULE_WIDGETS_EXPORT qSlicerLongPETCTReportTableWidget
+  : public QTableWidget
+{
+  Q_OBJECT
+
+public:
+  typedef QTableWidget Superclass;
+  qSlicerLongPETCTReportTableWidget(QWidget *parent=0);
+  virtual ~qSlicerLongPETCTReportTableWidget();
+
+
+public slots:
+  void update(vtkMRMLNode* node);
+
+protected slots:
+  void selectStudyColumn(int index);
+
+
+protected:
+  QScopedPointer<qSlicerLongPETCTReportTableWidgetPrivate> d_ptr;
+
+  virtual void resizeEvent(QResizeEvent* event);
+  void arrangeColumns();
+
+private:
+  Q_DECLARE_PRIVATE(qSlicerLongPETCTReportTableWidget);
+  Q_DISABLE_COPY(qSlicerLongPETCTReportTableWidget);
+
+
+
+};
+
+#endif
