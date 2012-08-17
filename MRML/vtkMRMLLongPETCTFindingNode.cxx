1bcfec8e91517c5803749ef37930132bda267190
Paul <Paul@Pauls-MacBook-Pro.local>
2012-08-17 18:19:41 -0400

    ENH: Added FindingNode to MRML

---
+++ MRML/vtkMRMLLongPETCTFindingNode.cxx
@@ -0,0 +1,146 @@
+/*=auto=========================================================================
+
+Portions (c) Copyright 2005 Brigham and Women\"s Hospital (BWH) All Rights Reserved.
+
+See COPYRIGHT.txt
+or http://www.slicer.org/copyright/copyright.txt for details.
+
+Program:   3D Slicer
+Module:    $RCSfile: vtkMRMLReprtingFindingNode.cxx,v $
+Date:      $Date: 2006/03/17 15:10:10 $
+Version:   $Revision: 1.2 $
+
+=========================================================================auto=*/
+
+// VTK includes
+#include <vtkCommand.h>
+#include <vtkObjectFactory.h>
+
+#include <map>
+
+
+// MRML includes
+#include "vtkMRMLLongPETCTFindingNode.h"
+
+#include "vtkMRMLLongPETCTStudyNode.h"
+
+#include <vtkNew.h>
+#include <vtkSmartPointer.h>
+
+#include <vtkMRMLAnnotationROINode.h>
+#include <vtkMRMLScalarVolumeNode.h>
+// STD includes
+
+
+//----------------------------------------------------------------------------
+vtkMRMLNodeNewMacro(vtkMRMLLongPETCTFindingNode);
+
+//----------------------------------------------------------------------------
+vtkMRMLLongPETCTFindingNode::vtkMRMLLongPETCTFindingNode()
+{
+  this->SetHideFromEditors(false);
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLLongPETCTFindingNode::~vtkMRMLLongPETCTFindingNode()
+{
+
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLLongPETCTFindingNode::ReadXMLAttributes(const char** atts)
+{
+  int disabledModify = this->StartModify();
+
+  Superclass::ReadXMLAttributes(atts);
+
+
+    this->EndModify(disabledModify);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLLongPETCTFindingNode::WriteXML(ostream& of, int nIndent)
+{
+  Superclass::WriteXML(of, nIndent);
+}
+
+//----------------------------------------------------------------------------
+// Copy the node\"s attributes to this object.
+// Does NOT copy: ID, FilePrefix, Name, SliceID
+void vtkMRMLLongPETCTFindingNode::Copy(vtkMRMLNode *anode)
+{
+  int disabledModify = this->StartModify();
+  
+  Superclass::Copy(anode);
+
+  this->EndModify(disabledModify);
+}
+
+//----------------------------------------------------------------------------
+void vtkMRMLLongPETCTFindingNode::PrintSelf(ostream& os, vtkIndent indent)
+{
+  Superclass::PrintSelf(os,indent);
+}
+
+//----------------------------------------------------------------------------
+bool vtkMRMLLongPETCTFindingNode::AddROIForStudy(vtkMRMLLongPETCTStudyNode* study, vtkMRMLAnnotationROINode* roi)
+{
+    return this->StudyAndROIMap.insert(std::make_pair(study,roi)).second; // "second" points to bool in std::pair return value of insert
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLAnnotationROINode* vtkMRMLLongPETCTFindingNode::RemoveROIForStudy(const vtkMRMLLongPETCTStudyNode* study)
+{
+  vtkMRMLAnnotationROINode* roiToRemove = this->GetROIForStudy(study);
+
+  if( roiToRemove != NULL)
+      this->StudyAndROIMap.erase(const_cast<vtkMRMLLongPETCTStudyNode*>(study));
+
+  return roiToRemove;
+}
+
+
+//----------------------------------------------------------------------------
+bool vtkMRMLLongPETCTFindingNode::AddLabelMapForROI(vtkMRMLAnnotationROINode* roi, vtkMRMLScalarVolumeNode* labelMapVolume)
+{
+    return this->ROIandLabelsMap.insert(std::make_pair(roi,labelMapVolume)).second; // "second" points to bool in std::pair return value of insert
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLScalarVolumeNode* vtkMRMLLongPETCTFindingNode::RemoveLabelMapForROI(const vtkMRMLAnnotationROINode* roi)
+{
+  vtkMRMLScalarVolumeNode* labelMapVolumeToRemove = this->GetLabelMapVolumeForROI(roi);
+
+  if( labelMapVolumeToRemove != NULL )
+    this->ROIandLabelsMap.erase(const_cast<vtkMRMLAnnotationROINode*>(roi));
+
+  return labelMapVolumeToRemove;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLAnnotationROINode* vtkMRMLLongPETCTFindingNode::GetROIForStudy(const vtkMRMLLongPETCTStudyNode* study)
+{
+  vtkMRMLAnnotationROINode* roi = NULL;
+
+  if(this->StudyAndROIMap.find(const_cast<vtkMRMLLongPETCTStudyNode*>(study)) != this->StudyAndROIMap.end())
+    {
+        roi = this->StudyAndROIMap[const_cast<vtkMRMLLongPETCTStudyNode*>(study)];
+    }
+
+  return roi;
+}
+
+//----------------------------------------------------------------------------
+vtkMRMLScalarVolumeNode* vtkMRMLLongPETCTFindingNode::GetLabelMapVolumeForROI(const vtkMRMLAnnotationROINode* roi)
+{
+  vtkMRMLScalarVolumeNode* labelMapVolume = NULL;
+
+  if(this->ROIandLabelsMap.find(const_cast<vtkMRMLAnnotationROINode*>(roi)) != this->ROIandLabelsMap.end())
+    {
+      labelMapVolume = this->ROIandLabelsMap[const_cast<vtkMRMLAnnotationROINode*>(roi)];
+    }
+
+  return labelMapVolume;
+}
+
+
---
+++ MRML/vtkMRMLLongPETCTFindingNode.h
@@ -0,0 +1,94 @@
+/*=auto=========================================================================
+
+  Portions (c) Copyright 2005 Brigham and Women's Hospital (BWH) All Rights Reserved.
+
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Program:   3D Slicer
+  Module:    $RCSfile: vtkMRMLVolumeRenderingParametersNode.h,v $
+  Date:      $Date: 2006/03/19 17:12:29 $
+  Version:   $Revision: 1.3 $
+
+=========================================================================auto=*/
+// .NAME vtkMRMLLongPETCTFindingNode
+// This node keeps pointers to the two elements of the Finding: annotation
+// and markup.
+//
+// Annotation element will (eventually) contain the Finding template and the
+// user-initialized fields of the template.
+//
+// Markup element will point to the hierarchy of the markup elements.
+//
+// On export/import, this node will be used by the IO logic to determine how
+// to code the formatted Finding.
+// .SECTION Description
+// This node keeps pointers to the two elements of the Finding: annotation
+// and markup.
+//
+
+#ifndef __vtkMRMLLongPETCTFindingNode_h
+#define __vtkMRMLLongPETCTFindingNode_h
+
+// MRML includes
+//#include "vtkDoubleArray.h"
+#include "vtkMRML.h"
+#include "vtkMRMLNode.h"
+
+#include <vtkSlicerLongPETCTModuleMRMLExport.h>
+
+class map;
+
+class vtkMRMLLongPETCTStudyNode;
+class vtkMRMLAnnotationROINode;
+class vtkMRMLScalarVolumeNode;
+
+#include <vtkMRMLAnnotationROINode.h>
+#include <vtkNew.h>
+#include <vtkSmartPointer.h>
+/// \ingroup Slicer_QtModules_LongPETCTFindingNode
+class VTK_SLICER_LONGPETCT_MODULE_MRML_EXPORT vtkMRMLLongPETCTFindingNode : public vtkMRMLNode
+{
+  public:   
+
+  static vtkMRMLLongPETCTFindingNode *New();
+  vtkTypeMacro(vtkMRMLLongPETCTFindingNode,vtkMRMLNode);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  virtual vtkMRMLNode* CreateNodeInstance();
+
+  /// Set node attributes
+  virtual void ReadXMLAttributes( const char** atts);
+
+  /// Write this node's information to a MRML file in XML format.
+  virtual void WriteXML(ostream& of, int indent);
+
+  /// Copy the node's attributes to this object
+  virtual void Copy(vtkMRMLNode *node);
+
+  /// Get node XML tag name (like Volume, Model)
+  virtual const char* GetNodeTagName() {return "Finding";};
+
+  bool AddROIForStudy(vtkMRMLLongPETCTStudyNode* study, vtkMRMLAnnotationROINode* roi);
+  vtkMRMLAnnotationROINode* RemoveROIForStudy(const vtkMRMLLongPETCTStudyNode* study);
+
+  bool AddLabelMapForROI(vtkMRMLAnnotationROINode* roi, vtkMRMLScalarVolumeNode* labelMapVolume);
+  vtkMRMLScalarVolumeNode* RemoveLabelMapForROI(const vtkMRMLAnnotationROINode* roi);
+
+  vtkMRMLAnnotationROINode* GetROIForStudy(const vtkMRMLLongPETCTStudyNode* study);
+  vtkMRMLScalarVolumeNode* GetLabelMapVolumeForROI(const vtkMRMLAnnotationROINode* roi);
+
+protected:
+  vtkMRMLLongPETCTFindingNode();
+  ~vtkMRMLLongPETCTFindingNode();
+  vtkMRMLLongPETCTFindingNode(const vtkMRMLLongPETCTFindingNode&);
+  void operator=(const vtkMRMLLongPETCTFindingNode&);
+
+
+  std::map<vtkMRMLLongPETCTStudyNode*,vtkMRMLAnnotationROINode*> StudyAndROIMap;
+  std::map<vtkMRMLAnnotationROINode*,vtkMRMLScalarVolumeNode*> ROIandLabelsMap;
+
+};
+
+#endif
+
